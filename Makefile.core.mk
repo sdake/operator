5dbdbc4ef53cc5f5a0ad3b357efb1bbac7ca71fe
Steven Dake <sdake@ibm.com>
2019-10-14 12:37:45 -0700

    Output to GOOS_GOARCH/release/docker

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -79,8 +79,8 @@ docker.push:
 	docker push $(HUB)/operator:$(TAG)
 
 docker.save: docker
-	mkdir -p $(TARGET_OUT)_release/docker
-	docker save $(HUB)/operator:$(TAG) -o $(TARGET_OUT)_release/docker/operator.tar
+	mkdir -p $(TARGET_OUT)/release/docker
+	docker save $(HUB)/operator:$(TAG) -o $(TARGET_OUT)/release/docker/operator.tar
 
 docker.all: docker docker.push
 
